9b3540f9a448eb227ca253c08e6f5551e6d26d4b
s.karekkattumanasree <s.karekkattumanasree@CA-L9Z2KNN2.groupinfra.com>
2021-04-23 10:42:08 -0700

    Changing to web client

--- api/src/main/java/ca/bc/gov/educ/api/program/EducGradProgramManagementApiApplication.java
+++ api/src/main/java/ca/bc/gov/educ/api/program/EducGradProgramManagementApiApplication.java
@@ -14,6 +14,7 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import ca.bc.gov.educ.api.program.model.dto.GradProgram;
 import ca.bc.gov.educ.api.program.model.entity.GradProgramEntity;
@@ -38,6 +39,11 @@ public class EducGradProgramManagementApiApplication {
 		modelMapper.typeMap(GradProgramEntity.class, GradProgram.class);
 		modelMapper.typeMap(GradProgram.class, GradProgramEntity.class);
 		return modelMapper;
+	}
+	
+	@Bean
+	public WebClient webClient() {
+		return WebClient.create();
 	}
 	
 	@Bean
